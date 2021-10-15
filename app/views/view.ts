bb97249cd17aec75c66d6cba5f0b0dbf17550761
Rafael <rafa.s.francisco@gmail.com>
2021-10-15 20:17:36 -0300

    aplicando classe abstrata e metodo abstrato

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,4 +1,4 @@
-export class View<T> {
+export abstract class View<T> {
 
     protected elemento: HTMLElement
 
@@ -6,9 +6,7 @@ export class View<T> {
         this.elemento = document.querySelector(seletor)
     }
 
-    template(model: T): string {
-        throw new Error('Tem que sobrescrever o método template')
-    }
+    abstract template(model: T): string
 
     update(model: T): void {
         this.elemento.innerHTML = this.template(model)
--- dist/js/views/view.js
+++ dist/js/views/view.js
@@ -2,9 +2,6 @@ export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-    template(model) {
-        throw new Error('Tem que sobrescrever o método template');
-    }
     update(model) {
         this.elemento.innerHTML = this.template(model);
     }
